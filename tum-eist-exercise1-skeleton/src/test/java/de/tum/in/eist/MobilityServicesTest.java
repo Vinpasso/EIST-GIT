9a75d7e08c32fe016508ebe41b236ed374b800e4
Vinpasso <vinpasso@vinpasso.org>
2015-06-07 21:28:41 +0200

    Test 1

--- tum-eist-exercise1-skeleton/src/test/java/de/tum/in/eist/MobilityServicesTest.java
+++ tum-eist-exercise1-skeleton/src/test/java/de/tum/in/eist/MobilityServicesTest.java
@@ -2,9 +2,31 @@ package de.tum.in.eist;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.lang.reflect.Method;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import de.tum.in.eist.rentalcar.RentalCarAPI;
+
+@RunWith(JUnit4.class)
 public class MobilityServicesTest {
 
+	@Test
+	public void testGetCarClass() throws Exception
+	{
+		Method method = MobilityServices.class.getDeclaredMethod("getCarClass", String.class);
+		method.setAccessible(true);
+		assertEquals("getCarClass Student", "Bike", method.invoke(null, "student"));
+		assertEquals("getCarClass Student", "Bike", method.invoke(null, "Student"));
+		assertEquals("getCarClass Student", "Bike", method.invoke(null, "STUDENT"));
+		assertEquals("getCarClass Business", "PremiumCar", method.invoke(null, "business"));
+		assertEquals("getCarClass Business", "PremiumCar", method.invoke(null, "Business"));
+		assertEquals("getCarClass Business", "PremiumCar", method.invoke(null, "BUSINESS"));
+		assertEquals("getCarClass Family", "MidsizeSUV", method.invoke(null, "family"));
+		assertEquals("getCarClass Family", "MidsizeSUV", method.invoke(null, "Family"));
+		assertEquals("getCarClass Family", "MidsizeSUV", method.invoke(null, "FAMILY"));
+}
 
 }
